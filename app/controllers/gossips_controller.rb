7bc11e5921c32d5beb0bf6a4813f670400a582c8
notmoebius <notmoebius@gmail.com>
2019-04-30 23:41:43 +0200

    update with new but KO

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,9 +1,18 @@
 class GossipsController < ApplicationController
   def new
-    @gossips = Gossip.all
+    @gossip = Gossip.new
   end
 
   def create
+    @gossip = Gossip.new('title' => params[:title], 'content' => params[:content], user_id: '1')
+
+    if @gossip.save
+      puts "The super potin was succesfully saved !"
+      redirect_to '/'
+    else
+      puts "Error : you need to complete this field / the title must be at least 3 characters longue / etc."
+      render'/gossips/new'
+    end
   end
 
   def update 
@@ -16,6 +25,7 @@ class GossipsController < ApplicationController
   end
 
   def index
+    @gossip = Gossip.all
   end
 
   def show
