a91a9c454d7ffb8ba340f91e1c90333deba49022
notmoebius <notmoebius@gmail.com>
2019-05-01 08:29:11 +0200

    tout fonctionne avec new

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -1,5 +1,8 @@
 class Gossip < ApplicationRecord
-    belongs_to :user, required: false
-    belongs_to :tag_gossip, required: false
-    has_many :tags, through: :tag_gossips
+  belongs_to :user, required: false
+  belongs_to :tag_gossip, required: false
+  has_many :tags, through: :tag_gossips
+
+  validates :title, presence: true, length: { minimum: 3, maximum: 14, message: "title need 3 car. or less than 14 car"} 
+  validates :content,presence: true
 end
