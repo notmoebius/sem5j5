318f9988f37ee699d35e90583897a43b89dec9a0
notmoebius <notmoebius@gmail.com>
2019-05-02 11:15:05 +0200

    add colum password_digest

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -4,6 +4,6 @@ class Gossip < ApplicationRecord
   has_many :tags, through: :tag_gossips
   has_many :comments
 
-  validates :title, presence: true, length: { minimum: 3, maximum: 14, message: "title need 3 car. or less than 14 car"} 
-  validates :content,presence: true
+  #validates :title, presence: true, length: { minimum: 3, maximum: 14, message: "title need 3 car. or less than 14 car"} 
+  #validates :content,presence: true
 end
